9b3b481c636efda740eed90c042d56d13817b04f
Parsa-vaezi2013 <parsavaezi.1391.pv2024@example.com>
2025-08-07 12:57:36 +0330

    اواسط پروژه تلگرام

--- tracker/serializer.py
+++ tracker/serializer.py
@@ -1,6 +1,5 @@
-from attr import fields
 from rest_framework import serializers
-from . models import Profile
+from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
--- tracker/tasks.py
+++ tracker/tasks.py
@@ -2,6 +2,21 @@ import threading
 import time
 from .models import Profile
 from telegram_bot.bot import send_alert
+import requests
+from django.conf import settings  # اگه از settings.py توکن رو بگیری
+
+BOT_TOKEN = '8171301737:AAGUb8GQ6M8TqIvOFARMpCJjVIz7hnhry00'
+
+def get_follower_count(channel_username):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/getChatMembersCount?chat_id=@{channel_username}"
+    try:
+        response = requests.get(url)
+        data = response.json()
+        return data.get("result", 0)
+    except Exception as e:
+        print("خطا در دریافت تعداد اعضا:", e)
+        return 0
+
 
 def check_falowers():
     while True:
